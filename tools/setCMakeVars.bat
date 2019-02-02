cb70dd3d4e188425e822b5fc89e4aad9a57f57c3
Isao Hara <haraisao@gmail.com>
2019-02-03 06:53:26 +0900

    Add unsetnv

--- tools/setCMakeVars.bat
+++ tools/setCMakeVars.bat
@@ -6,7 +6,7 @@ set ROS_DISTRO=melodic
 set ROS_HOME=%~d0\\opt\\ros\\%ROS_DISTRO%
 set ROS_HOME_CMAKE=%~d0/opt/ros/%ROS_DISTRO%
 
-set PYTHONPATH=%ROS_HOME%\Lib\site-packages\;%PYTHON_DIR%\Lib\site-packages%PYTHONPATH%
+set PYTHONPATH=%ROS_HOME%\Lib\site-packages\;%PYTHON_DIR%\Lib\site-packages;%PYTHONPATH%
 set PATH_ORG=%PATH%
 set PATH=%~dp0;%~d0\local\vim;%~d0\local\cmake-3.13.1-win64-x64\bin;%PYTHON_DIR%;%PYTHON_DIR%\Scripts;%PATH%
 
---
+++ tools/unsetenv.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set PYTHON_DIR=
+set ROS_DISTRO=
+
+set ROS_HOME=
+set ROS_HOME_CMAKE=
+set PYTHONPATH=
+set PATH=%PATH_ORG%
+
+set CMAKE_PREFIX_PATH=
+set BOOSTROOT=
+set TinyXML2_ROOT_DIR=
+
+set LZ4_ROOT=
+set BZIP2_DIR=
+rem set TinyXML_ROOT_DIR=%~d0/local/TinyXML
+set LLVM_INSTALL_DIR=
+set QTDIR=
+set Eigen3_DIR=
+set OpenCV_DIR=
+
+@echo on
