80cb0de0992ab7229952acf19960433ce190702b
Isao Hara <haraisao@gmail.com>
2019-02-04 15:20:37 +0900

    add check_repo

---
+++ check_repo.bat
@@ -0,0 +1,41 @@
+
+@echo off
+setlocal enabledelayedexpansion
+
+set PWD=%CD%
+set PKG_NAME=%1
+
+set PKG_LIST=%~dp0%PKG_NAME%\pkg_list.txt
+
+if "%1" == "" goto :error
+
+set GIT_DIFF=0
+FOR /F %%X in ( %PKG_LIST% ) do (
+  for /f "tokens=1,2 delims==" %%a in ("%%X") do (
+     cd %PKG_NAME%\%%a
+
+     for /f "tokens=1,2 delims=," %%p in ("%%b") do (
+       set FLAG=
+       FOR /F %%V in ('git diff') do (
+         if not "%%V" == "" ( set FLAG=1 ) 
+       )
+       if not "!FLAG!" == ""  (
+         set /A GIT_DIFF = !GIT_DIFF! + 1
+         echo === %%a ===
+         git diff
+       )
+     )
+     cd %PWD%
+  )
+)
+goto :end
+
+:error
+echo Usage: %0 [pkg]
+endlocal
+echo on
+@exit /b 1
+:end
+echo === %GIT_DIFF% repositories changed == 
+endlocal
+echo on
