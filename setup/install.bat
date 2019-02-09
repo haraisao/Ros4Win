5433e0a4219a5a15ae3c69d82dbf6147bc6b5f89
Isao Hara <haraisao@gmail.com>
2019-02-09 20:51:41 +0900

    Update install script.

--- setup/install.bat
+++ setup/install.bat
@@ -6,10 +6,10 @@ if "y%ROS_HOME%" == "y" goto :END
 
 xcopy /E opt\*.* %~d0\opt\
 xcopy /E scripts %ROS_HOME%\scripts\
-copy /Y ros_setup.bat %ROS_HOME%
-copy /Y env.bat %ROS_HOME%
-copy /Y setup.bat %ROS_HOME%
-copy /Y _setup_util.py %ROS_HOME%
+xcopy /E /Y ros_setup.bat %ROS_HOME%
+xcopy /E /Y env.bat %ROS_HOME%
+xcopy /E /Y setup.bat %ROS_HOME%
+xcopy /E /Y _setup_util.py %ROS_HOME%
 
 call installPyCmd.bat pycmd_list.txt
 :END
